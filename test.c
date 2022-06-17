d89b5a0f1a5dc8fd54dc574d26e5f5fd4f8b7ee1
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-06-17 10:23:59 +0600

    16.6.22.1

--- test.c
+++ test.c
@@ -1,27 +1,40 @@
 #include<stdio.h>
 
 int main (){
-    int a,b,c;
-    printf("Enter Three numbers:");
-    scanf("%d%d%d",&a,&b,&c);
-   if(a>=b)
-   {
+    int a,b,c,d;
+    printf("Enter Four numbers:");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+if(a>=b)
+{
     if(a>=c)
     {
-        printf("%d is the biggest number",a);
-    }else{
+        if(a>=d)
+        {
+            printf("%d is the biggest number",a);
+        }else{
+            printf("%d is the biggest number",d);
+        }
+    }else if (c>=d)
+    {
         printf("%d is the biggest number",c);
+    }else{
+        printf("%d is the biggest number",d);
     }
-   }else if (b>=c)
-   {
-    printf("%d is the biggest number",b);
-   }else{
-    printf("%d is the biggest number",c);
-   }
-   
     
-
-
+}else if (b>=c)
+{
+    if(b>=d){
+        printf("%d is the biggest number",b);
+    }else{
+        printf("%d is the biggest number",d);
+    }
+    
+}else if (c>=d)
+    {
+        printf("%d is the biggest number",c);
+    }else{
+        printf("%d is the biggest number",d);
+    }
     return 0;
 }
 
