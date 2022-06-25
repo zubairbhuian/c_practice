67069d641d7a0bc29f5bc8e07c51a4ffa4344f41
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-06-25 10:19:44 +0600

    121323

--- test.c
+++ test.c
@@ -1,26 +1,17 @@
+// A number is valid if it is positive or even. Take input from user and check its validity
 #include<stdio.h>
 
-int main (){
-    int a,b,c;
-    printf("Enter Three numbers:");
-    scanf("%d%d%d",&a,&b,&c);
-   if(a>=b)
-   {
-    if(a>=c)
-    {
-        printf("%d is the biggest number",a);
-    }else{
-        printf("%d is the biggest number",c);
-    }
-   }else if (b>=c)
-   {
-    printf("%d is the biggest number",b);
-   }else{
-    printf("%d is the biggest number",c);
-   }
-   
-    
 
+int main (){
+    int a;
+    printf("Enter a numbers:");
+    scanf("%d",&a);
+
+    if(a>=0 && a%2==0)
+        printf("%d valid",a);
+    else
+        printf("%d Not valid",a);
+    
 
     return 0;
 }
